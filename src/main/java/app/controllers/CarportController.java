fa4b31151c9856052c76b49d586c9b9939889b0f
clsch <alexsmed1@hotmail.com>
2026-05-11 10:01:39 +0200

    Changed the look of carportSkaber.html to look more like the other pages

--- src/main/java/app/controllers/CarportController.java
+++ src/main/java/app/controllers/CarportController.java
@@ -1,4 +1,16 @@
 package app.controllers;
 
+import app.persistence.ConnectionPool;
+import io.javalin.Javalin;
+
 public class CarportController {
+
+    public static void addRoutes(Javalin app, ConnectionPool connectionPool) {
+        app.get("/carportSkaber", ctx -> ctx.render("carportSkaber.html"));
+       // app.post("/createCarport", ctx -> createCarport(ctx, connectionPool));
+    }
+
+    public void createCarport(){
+
+    }
 }
