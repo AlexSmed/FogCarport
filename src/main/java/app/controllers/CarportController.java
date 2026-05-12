6cadef8844f7df68b9e8e125deead09eca0c22bd
clsch <alexsmed1@hotmail.com>
2026-05-12 12:47:44 +0200

    CarportController method orderCarport

--- src/main/java/app/controllers/CarportController.java
+++ src/main/java/app/controllers/CarportController.java
@@ -1,20 +1,35 @@
 package app.controllers;
 
+import app.entities.Stykliste;
+import app.entities.Users;
+import app.exception.DatabaseException;
+import app.persistence.CarportMapper;
 import app.persistence.ConnectionPool;
 import io.javalin.Javalin;
+import io.javalin.http.Context;
 
 public class CarportController {
     public static void addRoutes(Javalin app, ConnectionPool connectionPool) {
         app.get("/carportSkaber", ctx -> ctx.render("carportSkaber.html"));
 
-        app.post("/order", ctx -> orderCarport(ctx));
+        app.post("/order", ctx -> orderCarport(ctx, connectionPool));
     }
-    public static void orderCarport(Context ctx){
-        int width = Integer.parseInt(ctx.formParam("width"));
-        int length = Integer.parseInt(ctx.formParam("length"));
+    public static void orderCarport(Context ctx, ConnectionPool connectionPool){
 
-        System.out.println(width);
-        System.out.println(length);
-        ctx.render("carportSkaber.html");
+      try {
+          int width = Integer.parseInt(ctx.formParam("width"));
+          int length = Integer.parseInt(ctx.formParam("length"));
+          double pris = 0;
+          String status = "forspørglse afsendt";
+          Users user = ctx.sessionAttribute("currentUser");
+          int bruger_id = user.getUser_id();
+          int stykliste_id = 0;
+
+
+          CarportMapper.createCarport(width, length, pris, status, bruger_id, stykliste_id, connectionPool);
+          ctx.result("Carport oprettet!");
+      }catch (DatabaseException e) {
+          ctx.result("Fejl: " + e.getMessage());
+        }
     }
 }
